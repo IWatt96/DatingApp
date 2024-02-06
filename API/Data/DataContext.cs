db2fdd13efcbf6cb44d8c506646e6adc6474ba5a
IWatt96 <iainwatt96@gmail.com>
2024-02-05 21:09:34 -0500

    End of Section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{ // BASE REFERS TO DbContext, OPTIONS LEFT OPEN TO BE PROVIDED LATER.
+    public DataContext (DbContextOptions options) : base(options)
+    {
+    }
+
+    
+    public DbSet<AppUser> Users { get; set; }
+}
